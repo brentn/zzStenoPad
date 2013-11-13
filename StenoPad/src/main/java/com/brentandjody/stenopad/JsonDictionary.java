a8861f2b1fad0962de823652b21c35462f3d9e2d
brentn <nesbitt.brent@gmail.com>
2013-11-13 12:50:49 -0800

    initial work on Dictionary classes, and testing framework

---
+++ StenoPad/src/main/java/com/brentandjody/stenopad/JsonDictionary.java
@@ -0,0 +1,19 @@
+package com.brentandjody.stenopad;
+
+/**
+ * Created by brentn on 13/11/13.
+ */
+public class JsonDictionary {
+
+    public JsonDictionary() {
+        //TODO:
+    }
+
+    public void load() {
+        //TODO:
+    }
+
+    public void save() {
+        //TODO:
+    }
+}
---
+++ StenoPad/src/main/java/com/brentandjody/stenopad/RtfcreDictionary.java
@@ -0,0 +1,19 @@
+package com.brentandjody.stenopad;
+
+/**
+ * Created by brentn on 13/11/13.
+ */
+public class RtfcreDictionary {
+
+    public RtfcreDictionary() {
+        //TODO:
+    }
+
+    public void load() {
+        //TODO:
+    }
+
+    public void save() {
+        //TODO:
+    }
+}
